139cb73f3298cc46472dae68e70ae163c0e7f96b
Rohan <rohandute1@gmail.com>
2023-10-23 12:58:35 +0530

    [rohan] added:- successfully implemented option check

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,32 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder program.");
+        final int LADDER = 1;
+        final int SNAKE = 2;
+        int playerPosition = 0;
+
         int output=(int)(Math.random() * 6) + 1;
         System.out.println("The dice number is: "+ output);
+
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+
+        if (option == LADDER)
+        {
+            playerPosition = playerPosition + output;
+            System.out.println("Your status is ladder");
+            System.out.println("Your position is :" + playerPosition);
+        }
+        else if (option == SNAKE)
+        {
+            playerPosition = playerPosition - output;
+            System.out.println(" Your status is Snake");
+            System.out.println("Your position is :" + playerPosition);
+
+        }
+        else
+        {
+            System.out.println("Your status is no play :");
+            System.out.println("Your position is :" + playerPosition);
+        }
     }
 }
