276d353717cbb162bdff8d913d3347d0087a7cd5
Rohan <rohandute1@gmail.com>
2023-10-23 11:51:55 +0530

    [rohan] added:- successfully starts the game from initial 0 position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder program.");
+
+        System.out.println("You are starting at position 0.");
     }
 }
