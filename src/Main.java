d5089628cf068dfa1248d7c665b3a50cbf4605bb
Rohan <rohandute1@gmail.com>
2023-10-23 12:45:06 +0530

    [rohan] added:- successfully implemented random

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder program.");
-
-        System.out.println("You are starting at position 0.");
+        int output=(int)(Math.random() * 6) + 1;
+        System.out.println("The dice number is: "+ output);
     }
 }
