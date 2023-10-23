267b2690012eced54141220f064c7de8fdb1f8c0
Rohan <rohandute1@gmail.com>
2023-10-23 14:13:33 +0530

    [rohan] added:- Added game logic to track dice rolls and positions

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,9 +6,11 @@ public class Main {
         final int LADDER = 1;
         final int SNAKE = 2;
         int playerPosition = 0;
+        int count = 0;
 
         while (playerPosition < 100){
             int output=(int)(Math.random() * 6) + 1;
+            count++;
             System.out.println("The dice number is: "+ output);
             int option = (int) Math.floor(Math.random() * 10) % 3;
 
@@ -36,6 +38,7 @@ public class Main {
                 System.out.println("Your position is :" + playerPosition);
             }
         }
+        System.out.println("Total number of times dice is played :" + count);
         System.out.println("Congratulations!");
         System.out.println("You reached the final position!");
         System.out.println("!Game over!");
