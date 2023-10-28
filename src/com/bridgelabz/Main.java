686f3f4acceffb6536c530117f317c6c63596c94
Rohan <rohandute1@gmail.com>
2023-10-28 12:38:25 +0530

    [rohan] added:- successfully implement two-player functionality and game logic

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,42 +5,67 @@ public class Main {
         System.out.println("Welcome to Snake and Ladder program.");
         final int LADDER = 1;
         final int SNAKE = 2;
-        int playerPosition = 0;
-        int count = 0;
+        int player1Position = 0;
+        int player2Position = 0;
+        int currentPlayer = 1;
+        int count1 = 0;
+        int count2 = 0;
 
-        while (playerPosition < 100){
-            int output=(int)(Math.random() * 6) + 1;
-            count++;
-            System.out.println("The dice number is: "+ output);
+        while (player1Position < 100 && player2Position < 100) {
+            int output = (int)(Math.random() * 6) + 1;
+            if (currentPlayer == 1) {
+                count1++;
+                System.out.println("Player 1, the dice number is: " + output);
+            } else {
+                count2++;
+                System.out.println("Player 2, the dice number is: " + output);
+            }
+
             int option = (int) Math.floor(Math.random() * 10) % 3;
 
-            if (option == LADDER)
-            {
-                if (playerPosition + output <= 100)
-                {
-                    playerPosition = playerPosition + output;
-                    System.out.println("Your status is Ladder");
+            if (option == LADDER) {
+                if (currentPlayer == 1 && player1Position + output <= 100) {
+                    player1Position += output;
+                    System.out.println("Player 1, Your status is Ladder");
+                    System.out.println("Player 1, Your Position is: " + player1Position);
+                } else if (currentPlayer == 2 && player2Position + output <= 100) {
+                    player2Position += output;
+                    System.out.println("Player 2, Your status is Ladder");
+                    System.out.println("Player 2, Your Position is: " + player2Position);
+                }
+            } else if (option == SNAKE) {
+                if (currentPlayer == 1 && player1Position > 0 && (player1Position - output) >= 0) {
+                    player1Position -= output;
+                    System.out.println("Player 1, Your status is Snake");
+                    System.out.println("Player 1, Your Position is: " + player1Position);
+                } else if (currentPlayer == 2 && player2Position > 0 && (player2Position - output) >= 0) {
+                    player2Position -= output;
+                    System.out.println("Player 2, Your status is Snake");
+                    System.out.println("Player 2, Your Position is: " + player2Position);
+                }
+            } else {
+                if (currentPlayer == 1) {
+                    System.out.println("Player 1, Your status is no play :");
+                    System.out.println("Player 1, Your position is: " + player1Position);
+                } else {
+                    System.out.println("Player 2, Your status is no play :");
+                    System.out.println("Player 2, Your position is: " + player2Position);
+                }
+            }
 
-                    System.out.println("Your Position is :" + playerPosition);
-                }
-            }
-            else if (option == SNAKE)
-            {
-                if (playerPosition > 0 && (playerPosition - output) >= 0) {
-                    playerPosition = playerPosition - output;
-                    System.out.println(" Your status is Snake");
-                    System.out.println("Your Position is :" + playerPosition);
-                }
-            }
-            else
-            {
-                System.out.println("Your status is no play :");
-                System.out.println("Your position is :" + playerPosition);
-            }
+            // Switch to the other player's turn
+            currentPlayer = (currentPlayer == 1) ? 2 : 1;
         }
-        System.out.println("Total number of times dice is played :" + count);
+
+        if (player1Position >= 100) {
+            System.out.println("Player 1 wins!");
+        } else {
+            System.out.println("Player 2 wins!");
+        }
+
+        System.out.println("Total number of times dice is played for Player 1: " + count1);
+        System.out.println("Total number of times dice is played for Player 2: " + count2);
         System.out.println("Congratulations!");
-        System.out.println("You reached the final position!");
-        System.out.println("!Game over!");
+        System.out.println("Game over!");
     }
 }
